0492e34cffcfbd0eb5b648df5a5df463a7af9347
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-21 13:48:31 +0700

    Menambahkan daftar items berisi nama, gender, dan alamat untuk ditampilkan pada UI Siswa`

--- app/src/main/java/com/example/mvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/TampilSiswa.kt
@@ -31,5 +31,9 @@ fun TampilSiswa(
     statusUISiswa: Siswa,
     onBackButtonClicked: () -> Unit
 ){
-
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama), statusUISiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUISiswa.gender),
+        Pair(stringResource(id = R.string.alamat), statusUISiswa.alamat)
+    )
 }
