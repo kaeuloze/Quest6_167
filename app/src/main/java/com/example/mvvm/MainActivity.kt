5e3065dbcb4c4849b4f7af3a62804c24e850cbbf
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-21 14:53:04 +0700

    Memperbarui isi pada FormSiswa, TampilSiswa dan Main Activity

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvm.ui.theme.MVVMTheme
 import com.example.mvvm.view.uicontroller.SiswaApp
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
